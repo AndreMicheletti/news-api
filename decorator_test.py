e73cbb6c2ac3212b009537af6f4df0f3f75fa7f7
André Luiz Micheletti <andreluizmtmicheletti@gmail.com>
2017-07-06 16:54:41 -0300

    adding several decorator examples

--- decorator_test.py
+++ decorator_test.py
@@ -1,4 +1,72 @@
+"""
 
+ MY DECORATORS 
+
+Some decorator usages
+
+"""
+
+# Require Login
+def require_login(function):
+	def wrap(*args, **kw):
+		if(check_login()):
+			return function(*args, **kw)
+		else:
+			print("Not logged in")
+	return wrap
+		
+
+		
+# Check if file exists and if not, create
+def ensure_file(function):
+	def wrap(filename):
+		import os
+		if not(os.path.isfile(filename)):
+			with open(filename, 'w') as file:
+				file.write('')
+		return function(filename)
+	return wrap
+	
+	
+	
+# Same as before, but the filename is passed by the decorator (function arguments are free)
+def ensure_file2(filename):
+	def _decorator(function):
+		def wrap(*args, **kw):
+			import os
+			if not(os.path.isfile(filename)):
+				with open(filename, 'w') as file:
+					file.write('')
+			return function(*args, **kw)
+		return wrap
+	return _decorator
+
+	
+
+# Ensure that all string arguments are formatted 
+def string_format(function):
+
+	# define here how the string should be formatted
+	def _format(_str):
+		return _str.lower().strip()
+		
+	def wrap(*args, **kw):
+	
+		# format all strings in *arguments
+		args = [_format(arg) for arg in args if (isinstance(arg, str))]
+		
+		# format all strins in **keyword arguments
+		for key in kw.keys():
+			if (isinstance(kw[key], str)):
+				kw[key] = _format(kw[key])
+				
+		# execute the original function
+		return function(*args, **kw)
+	return wrap
+	
+	
+	
+# Cache a function's result
 class cached(object):
 
 	def __init__(self, func):
@@ -18,10 +86,22 @@ class cached(object):
 			return value
 
 			
+			
+# Time a function
+def timer(func):
+	def wrapper(*args, **kw):
+		import time
+		t = time.time()
+		result = func(*args, **kw)
+		print("call to {0} took {1:f} seconds".format(str(func.__name__), time.time() - t))
+		return result
+	return wrapper
+
+
+	
+# DECORATOR CLASS for maintaining profile history
 import os
 import json
-
-#DECORATOR CLASS for maintaining profile history
 class profile_and_save(object):
 	
 	def __init__(self, func):
@@ -48,19 +128,39 @@ class profile_and_save(object):
 		with open(self.filename, 'w') as f:
 			json.dump(self.data, f)
 		return result
+"""
 
+DECORATOR TESTS
 
-@profile_and_save
-@cached
-def fib(n):
-	a,b = 1,1
-	for i in range(n-1):
-		a,b = b,a+b
-	return a
+"""
+if (__name__ == "__main__"):
 
-if (__name__ == '__main__'):
-	fib(13000)
-	fib(13000)
+	def test_mystuff(benchmark):
+		benchmark(process)
+	
+	@timer
+	@cached
+	def large_function(n):
+		import time
+		time.sleep(1.1)
+		return n + 2
+		
+	def process():
+		large_function(2)
+		large_function(2)
+	
+	process()
+	
+	@string_format
+	def echo(*args, **kw):
+		for str in args:
+			print(str)
+		for str in kw.values():
+			print (str)
+		
+	echo("a ", "   b ")
+	echo(" D ")
+	echo(" EF                       ", arg1='           f        ', arg2=' G            ')
 
 """			
 https://www.turnkeylinux.org/blog/python-optimization
